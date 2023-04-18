ae90e87db04d044f3c29fc8934bb9069ec1b29cf
s222207 <125963880+s222207@users.noreply.github.com>
2023-04-18 23:14:49 +0200

    Update __init__.py

--- carbon_app/__init__.py
+++ carbon_app/__init__.py
@@ -1,16 +1,28 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 import os
+
 application = Flask(__name__)
 
-# = os.environ['SECRET_KEY]
-application.config['SECRET_KEY'] = '9295f1efe735f29d3b8a645260d191075ec8c0d09625826e'
+### Code GitHub
+application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
+application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
+application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
+
+
+db = SQLAlchemy(application)
+bcrypt = Bcrypt(application)
+login_manager= LoginManager(application)
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
 
 from carbon_app.home.routes import home
 from carbon_app.methodology.routes import methodology
-from carbon_app.carbon_calculator.routes import carbon_calculator
+# from carbon_app.carbon_app.routes import carbon_app
 from carbon_app.users.routes import users
 
 application.register_blueprint(home)
 application.register_blueprint(methodology)
-application.register_blueprint(carbon_calculator)
+# application.register_blueprint(carbon_app)
 application.register_blueprint(users)
