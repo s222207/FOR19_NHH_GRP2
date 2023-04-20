0d4b4df0cd867d0f053f4a496e03a4405b824a80
akselbjorhovde <126146616+akselbjorhovde@users.noreply.github.com>
2023-04-20 20:39:43 +0200

    Update forms.py

--- carbon_app/users/forms.py
+++ carbon_app/users/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from carbon_app.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField(
@@ -20,6 +21,17 @@ class RegistrationForm(FlaskForm):
         validators = [DataRequired(), EqualTo('password')]
     )
     submit = SubmitField('Sign Up')
+
+#Validate username and email
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username is already taken. Please choose a different one.')
+        
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError('A user is already registered with that email. Please log in or choose a different one.')    
 
 class LoginForm(FlaskForm):
     email = StringField(
